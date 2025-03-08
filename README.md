f8dff9d9cf2faecee076e38914736cd0fbb46d96
王睿 <wangrui027@outlook.com>
2025-03-08 19:27:59 +0800

    首次提交源码

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # xxl-job-spring-boot-starter
-xxl-job SpringBoot 启动器，引入依赖，配置参数开箱即用，无需手动创建配置类，注入 bean
+## 概述
+
+xxl-job SpringBoot 启动器，开箱即用。引入依赖，配置参数，立即开工，无需手动创建配置类和注入 bean。
+
+本项目源码：[https://github.com/wangrui027/xxl-job-spring-boot-starter](https://github.com/wangrui027/xxl-job-spring-boot-starter)
+
+xxl-job 首页：[https://www.xuxueli.com/xxl-job/](https://www.xuxueli.com/xxl-job/)
+
+xxl-job 源码：[https://github.com/xuxueli/xxl-job/](https://github.com/xuxueli/xxl-job/)
+
+## 使用方式
+
+**1、引入依赖**
+
+```xml
+<dependency>
+    <groupId>com.github.wangrui027</groupId>
+    <artifactId>xxl-job-spring-boot-starter</artifactId>
+    <version>2.5.0</version>
+</dependency>
+```
+
+**2、配置参数**
+
+application.yml 示例：
+
+```yaml
+xxl:
+  job:
+    admin:
+      addresses: http://xx.xx.xx.xx:8020/xxl-job-admin
+      accessToken: xxx
+      timeout: 3
+    executor:
+      appname: my_app
+      address: http://xx.xx.xx.xx:8011
+      ip: xx.xx.xx.xx
+      port: 8011
+      logpath: /data/applogs/xxl-job/jobhandler
+      logretentiondays: 30
+```
+
+application.properties 示例：
+
+```properties
+xxl.job.admin.addresses=http://xx.xx.xx.xx:8020/xxl-job-admin
+xxl.job.admin.accessToken=xxx
+xxl.job.admin.timeout=3
+xxl.job.executor.appname=my_app
+xxl.job.executor.address=http://xx.xx.xx.xx:8011
+xxl.job.executor.ip=xx.xx.xx.xx
+xxl.job.executor.port=8011
+xxl.job.executor.logpath=/data/applogs/xxl-job/jobhandler
+xxl.job.executor.logretentiondays=30
+```
+
+## 鸣谢
+
+感谢**徐雪里**同学为我们带来 `xxl-job` 这么简单好用的分布式任务调度平台。
---
+++ src/main/java/com/github/wangrui027/xxl/job/config/XxlJobAutoConfiguration.java
@@ -0,0 +1,41 @@
+package com.github.wangrui027.xxl.job.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+@Slf4j
+@Configuration
+@ConditionalOnClass(XxlJobSpringExecutor.class)
+@EnableConfigurationProperties(XxlJobProperties.class)
+public class XxlJobAutoConfiguration {
+
+    @Resource
+    private XxlJobProperties xxlJobProperties;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(xxlJobProperties.getAdmin().getAddresses());
+        xxlJobSpringExecutor.setAppname(xxlJobProperties.getExecutor().getAppname());
+        xxlJobSpringExecutor.setAddress(xxlJobProperties.getExecutor().getAddress());
+        xxlJobSpringExecutor.setIp(xxlJobProperties.getExecutor().getIp());
+        xxlJobSpringExecutor.setPort(xxlJobProperties.getExecutor().getPort());
+        xxlJobSpringExecutor.setAccessToken(xxlJobProperties.getAdmin().getAccessToken());
+        xxlJobSpringExecutor.setTimeout(xxlJobProperties.getAdmin().getTimeout());
+        xxlJobSpringExecutor.setLogPath(xxlJobProperties.getExecutor().getLogpath());
+        xxlJobSpringExecutor.setLogRetentionDays(xxlJobProperties.getExecutor().getLogretentiondays());
+        log.info(">>>>>>>>>>> xxl-job config init success.");
+        return xxlJobSpringExecutor;
+    }
+
+}
